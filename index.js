e0104a52d77d7d0e9909f942f61c028e3894ce84
isaacmogaka <isaacmogaka2016@gmail.com>
2023-06-12 11:55:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const names = ["Gilbert", "Isaac", "Mica"];
+const messages = writeCards(names);
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+
+  console.log(messages);
+  
+
+   var number1;
+   function countDown(number1){
+     for (let i = number1; i>=0; i--){
+         console.log(i);
+     }
+   }
+
+   countDown(number1);
